1e3a146cf9335e21411f324e80d378efc5cb117f
kipling <kip.crossing@gmail.com>
2021-01-05 16:55:06 +1100

    fix typing

--- geotiff/utils/geotiff_logging.py
+++ geotiff/utils/geotiff_logging.py
@@ -1,5 +1,5 @@
 import logging
-from colorlog import ColoredFormatter
+from colorlog import ColoredFormatter # type: ignore
 
 LOG_LEVEL = logging.DEBUG
 LOGFORMAT =   "(%(module)s) %(log_color)s%(levelname)s%(reset)s | %(log_color)s%(message)s%(reset)s"
@@ -8,6 +8,6 @@ formatter = ColoredFormatter(LOGFORMAT)
 stream = logging.StreamHandler()
 stream.setLevel(LOG_LEVEL)
 stream.setFormatter(formatter)
-log: logging.getLogger = logging.getLogger('pythonConfig')
+log = logging.getLogger('pythonConfig')
 log.setLevel(LOG_LEVEL)
 log.addHandler(stream)
