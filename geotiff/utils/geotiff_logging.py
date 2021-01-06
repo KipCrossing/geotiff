d5b71f84ec3fc40bd36d5541c47b330a9d407aba
kipling <kip.crossing@gmail.com>
2021-01-06 12:48:03 +1100

    Add logging back in

---
+++ geotiff/utils/geotiff_logging.py
@@ -0,0 +1,13 @@
+import logging
+from colorlog import ColoredFormatter # type: ignore
+
+LOG_LEVEL = logging.INFO
+LOGFORMAT =   "(%(module)s) %(log_color)s%(levelname)s%(reset)s | %(log_color)s%(message)s%(reset)s"
+logging.root.setLevel(LOG_LEVEL)
+formatter = ColoredFormatter(LOGFORMAT)
+stream = logging.StreamHandler()
+stream.setLevel(LOG_LEVEL)
+stream.setFormatter(formatter)
+log = logging.getLogger('pythonConfig')
+log.setLevel(LOG_LEVEL)
+log.addHandler(stream)
