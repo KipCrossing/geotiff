dc64ef437a691a4e3013fa8afa7f6045dd4c3da6
kipling <kip.crossing@gmail.com>
2021-07-15 14:00:26 +1000

    lift to name=main

--- example.py
+++ example.py
@@ -5,24 +5,24 @@ from geotiff import GeoTiff # type: ignore
 
 
 filename = "dem.tif"
-filename = "dem.tif"
+filename = "red.tif"
 dir = "./tests/inputs/"
 tiff_file = os.path.join(dir, filename)
-tiff_file = "/home/kipling/Programs/pylandsat_sandbox/data/gamma/Radmap2019-grid-k_conc-Filtered-AWAGS_RAD_2019.tif"
+# tiff_file = "/home/kipling/Programs/pylandsat_sandbox/data/gamma/Radmap2019-grid-k_conc-Filtered-AWAGS_RAD_2019.tif"
 # 138.632071411 -32.447310785 138.644218874 -32.456979174
 bounding_box: List[Tuple[float, float]] = [(138.632071411, -32.447310785), (138.644218874, -32.456979174)]
 
-
-print("testing read tiff")
-print(f"reading: {tiff_file}")
-print(f"Using bBox: {bounding_box}")
-geotiff: GeoTiff = GeoTiff(tiff_file, crs_code=4236)
-print(geotiff.tif_bBox)
-print(geotiff.crs_code)
-array = geotiff.read_box(bounding_box)
-print("Sample array:")
-print(array)
-print(array.shape)
-assert isinstance(array, np.ndarray)
-print(geotiff.tif_bBox_wgs_84)
-print("end")
+if __name__ == '__main__':
+    print("testing read tiff")
+    print(f"reading: {tiff_file}")
+    print(f"Using bBox: {bounding_box}")
+    geotiff: GeoTiff = GeoTiff(tiff_file)
+    print(geotiff.tif_bBox)
+    print(geotiff.crs_code)
+    array = geotiff.read_box(bounding_box)
+    print("Sample array:")
+    print(array)
+    print(array.shape)
+    assert isinstance(array, np.ndarray)
+    print(geotiff.tif_bBox_wgs_84)
+    print("end")
