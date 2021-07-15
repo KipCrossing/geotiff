e452b7a909a29ed815e28a8cc6e99b7707a6db4d
kipling <kip.crossing@gmail.com>
2021-07-15 16:17:40 +1000

    fix fixture name bug: geo_tiff

--- tests/test_geotiff.py
+++ tests/test_geotiff.py
@@ -6,7 +6,7 @@ from geotiff import GeoTiff
 import zarr
 
 @pytest.fixture(params=["dem.tif", "gda_94_sand.tif",  "sand_test.tif", "red.tif"])
-def geotiff(request):
+def geo_tiff(request):
     filename = request.param 
     dir = dir = "./tests/inputs/"
     # TODO: test bands, and crs params
@@ -18,39 +18,39 @@ def area_box():
     return ((138.632071411, -32.447310785), (138.644218874, -32.456979174))
 
 
-def test_read(geotiff: GeoTiff):
-    zarr_array = geotiff.read()
+def test_read(geo_tiff: GeoTiff):
+    zarr_array = geo_tiff.read()
     print(zarr_array.info)
     assert isinstance(zarr_array, zarr.Array)
     print(zarr_array.chunks)
 
-def test_read_box(area_box, geotiff: GeoTiff):
+def test_read_box(area_box, geo_tiff: GeoTiff):
     print("testing read tiff")
-    print(f"reading: {geotiff}")
+    print(f"reading: {geo_tiff}")
     print(f"Using bBox: {area_box}")
-    print(f"crs_code: {geotiff.crs_code}")
-    print(f"as_crs: {geotiff.as_crs}")
-    array = geotiff.read_box(area_box)
+    print(f"crs_code: {geo_tiff.crs_code}")
+    print(f"as_crs: {geo_tiff.as_crs}")
+    array = geo_tiff.read_box(area_box)
     print("Sample array:")
     print(array)
     print(array.shape)
     assert isinstance(array, np.ndarray)
 
-def test_get_coord_arrays(geotiff: GeoTiff, area_box):
-    array = geotiff.read_box(area_box)
-    print(geotiff.tif_bBox_wgs_84)
-    lon_array, lat_array = geotiff.get_coord_arrays(bBox=area_box)
+def test_get_coord_arrays(geo_tiff: GeoTiff, area_box):
+    array = geo_tiff.read_box(area_box)
+    print(geo_tiff.tif_bBox_wgs_84)
+    lon_array, lat_array = geo_tiff.get_coord_arrays(bBox=area_box)
     assert array.shape == lon_array.shape
     assert array.shape == lat_array.shape
 
 
-def test_int_box(area_box, geotiff: GeoTiff):
-    intBox = geotiff.get_int_box(area_box)
+def test_int_box(area_box, geo_tiff: GeoTiff):
+    intBox = geo_tiff.get_int_box(area_box)
     assert isinstance(intBox, tuple)
     assert len(intBox) == 2
     assert isinstance(intBox[0], tuple)
     assert isinstance(intBox[1], tuple)
-    intBox_outer = geotiff.get_int_box(area_box, outer_points=True)
+    intBox_outer = geo_tiff.get_int_box(area_box, outer_points=True)
     assert intBox[0][0] == intBox_outer[0][0]+1
     assert intBox[0][1] == intBox_outer[0][1]+1
     assert intBox[1][0] == intBox_outer[1][0]-1
@@ -58,12 +58,12 @@ def test_int_box(area_box, geotiff: GeoTiff):
 
 
 
-def test_conversions(area_box, geotiff: GeoTiff):
-    int_box = geotiff.get_int_box(area_box)
+def test_conversions(area_box, geo_tiff: GeoTiff):
+    int_box = geo_tiff.get_int_box(area_box)
     i = int_box[0][0] + 5
     j = int_box[0][1] + 6
-    geotiff.get_wgs_84_coords(i, j)
-    bounding_box = geotiff.get_bBox_wgs_84(area_box)
+    geo_tiff.get_wgs_84_coords(i, j)
+    bounding_box = geo_tiff.get_bBox_wgs_84(area_box)
 
     # * note: these tests will fail when the tiff it skewed
     # assert area_box[0][0] <= bounding_box[0][0]
@@ -71,7 +71,7 @@ def test_conversions(area_box, geotiff: GeoTiff):
     # assert area_box[1][0] >= bounding_box[1][0]
     # assert area_box[1][1] <= bounding_box[1][1]
 
-    bounding_box_outer = geotiff.get_bBox_wgs_84(area_box, outer_points=True)
+    bounding_box_outer = geo_tiff.get_bBox_wgs_84(area_box, outer_points=True)
 
     print(area_box)
 
@@ -80,7 +80,7 @@ def test_conversions(area_box, geotiff: GeoTiff):
     assert area_box[1][0] <= bounding_box_outer[1][0]
     assert area_box[1][1] >= bounding_box_outer[1][1]
 
-    bounding_box_outer2 = geotiff.get_bBox_wgs_84(area_box, outer_points=2)
+    bounding_box_outer2 = geo_tiff.get_bBox_wgs_84(area_box, outer_points=2)
 
     assert bounding_box_outer[0][0] >= bounding_box_outer2[0][0]
     assert bounding_box_outer[0][1] <= bounding_box_outer2[0][1]
